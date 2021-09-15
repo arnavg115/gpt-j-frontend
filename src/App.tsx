4be4719735aecc6d1bbaf310f97bb43283b589df
Arnav G <arnavg286@gamil.com>
2021-09-14 23:24:29 -0700

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,94 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardFooter,
+  CardHeader,
+  CheckBox,
+  Heading,
+  RangeInput,
+  Spinner,
+  TextArea,
+} from "grommet";
+import "./App.css";
+import axios, { AxiosResponse } from "axios";
+import { Launch } from "grommet-icons";
 function App() {
+  const [running, setRunning] = useState(false);
+  const [Advanced, setAdvanced] = useState(true);
+  const [context, setContext] = useState("");
+  const [res, setRes] = useState<AxiosResponse | undefined>();
+  const [temp, setTemp] = useState(0.1);
+  async function run() {
+    setRunning(true);
+    let res;
+    try {
+      res = await axios.post(process.env.REACT_APP_URL!, {
+        context: context,
+        temp: temp,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+    setRes(res);
+    setRunning(false);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="app">
+      <Card width="large" background="light">
+        <CardHeader pad="medium">
+          <Heading level="2" margin="none">
+            GPT-J
+          </Heading>
+        </CardHeader>
+        <CardBody pad="medium">
+          <TextArea
+            placeholder="Context to generate"
+            disabled={running}
+            value={context}
+            onChange={(e) => {
+              setContext(e.target.value);
+            }}
+          />
+          <p>
+            <strong>{context}</strong>
+            {res ? res.data.text : null}
+          </p>
+          <p>Advanced Options</p>
+          <CheckBox
+            checked={Advanced}
+            onChange={() => setAdvanced(!Advanced)}
+            disabled={running}
+          />
+          {Advanced ? (
+            <div>
+              <p>Temperature: {temp}</p>
+              <RangeInput
+                max={1}
+                min={0.01}
+                step={0.01}
+                value={temp}
+                onChange={(e) => setTemp(parseFloat(e.target.value))}
+                disabled={running}
+              />
+            </div>
+          ) : null}
+        </CardBody>
+        <CardFooter
+          pad="small"
+          background="light"
+          style={{ display: "flex", justifyContent: "center" }}
         >
-          Learn React
-        </a>
-      </header>
+          <Button
+            label={running ? "Running" : "Run"}
+            hoverIndicator
+            icon={running ? <Spinner /> : <Launch />}
+            disabled={running}
+            onClick={run}
+          />
+        </CardFooter>
+      </Card>
     </div>
   );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import { Grommet } from "grommet";
+
+const customTheme = {
+  global: {
+    colors: {
+      // Overriding existing grommet colors
+      brand: "#1E3D58",
+      focus: "#43B0F1",
+      light: "#E8EEF1",
+    },
+  },
+};
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Grommet theme={customTheme}>
+      <App />
+    </Grommet>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
